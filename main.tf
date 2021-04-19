af9e9e96f3451ccb7e08ed649bae55ca7f990915
Andrew Bulford <andrew.bulford@redmatter.com>
2021-04-19 17:24:14 +0100

    Return `version_override` variable value if set
    This allows users of the module to override the version value to be
    returned by passing it in as a variable.
    
    This can be helpful if the version can be passed to the parent module
    via a variable, saving the parent module from needing to implement logic
    to optionally call the module.
    
    AWS-582

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
 data "external" "version" {
+  count   = var.version_override == null ? 1 : 0
   program = ["python", "${path.module}/git_describe.py", var.version_environment_variable]
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "version_environment_variable" {
   default     = "TF_MODULE_VERSION"
   description = "Name of the environment variable to use as the version to be returned, if set"
 }
+
+variable "version_override" {
+  default     = null
+  description = "Version to return instead of using Git or the environment"
+}
